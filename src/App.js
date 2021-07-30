3ff378583c999fc8052fc3ea84e76a8bfa9c8249
agiya21 <agiya72@gmail.com>
2021-07-30 12:19:07 +0700

    feat: 🎸 Erase WeatherDetail and adding class to WeatherCard
    Erasing WeatherDetail component and adding class file to WeatherCard.
    Also, adding state to WeatherCard, so props can be saved in state.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import SearchBar from './components/SearchBar'
 import WeatherCard from './components/WeatherCard'
-import WeatherDetails from './components/WeatherDetails'
 
 function App() {
   return (
     <div>
       <Header />
       <SearchBar />
-      <WeatherCard city="Jakarta"/>
-      
-      <WeatherDetails />
+      <WeatherCard city="Malang"/>
       <Footer />
     </div>
   )
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -5,17 +5,25 @@ import Temperature from './WeatherCard/Temperature'
 import Conditions from './WeatherCard/Conditions'
 import UpdateTime from './WeatherCard/UpdateTime'
 
-function WeatherCard(props) {
-    return(
-        <div>
-            <CityName name={props.city}/>
-            <Pics />
-            <Temperature tempCelsius="25 Celsius"
-                tempFahrenheit="77 Fahrenheit"/>
-            <Conditions conds="Rainy"/>
-            <UpdateTime />
-        </div>
-    )
+class WeatherCard extends React.Component {
+    constructor(props){
+        super()
+        this.state = {
+            cityName : props.city
+        }
+    }
+    render() {
+        return (
+            <div>
+                <CityName name={this.state.cityName} />
+                <Pics />
+                <Temperature tempCelsius="25 Celsius"
+                    tempFahrenheit="77 Fahrenheit" />
+                <Conditions conds="Rainy" />
+                <UpdateTime />
+            </div>
+        )
+    }
 }
 
 export default WeatherCard
