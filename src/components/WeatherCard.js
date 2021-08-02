3c2f00a0f5877c57df3c75c5105f6895f8b8cef6
agiya21 <agiya72@gmail.com>
2021-08-02 17:16:34 +0700

    feat: 🎸 Fetching API
    Fetching API from openweathermap in WeatherCard, so system can fetch
    weather information and show it on Weather Card

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -9,17 +9,44 @@ class WeatherCard extends React.Component {
     constructor(props){
         super()
         this.state = {
-            cityName : props.city
+            cityName : props.city,
+            weatherDetails : {}
         }
     }
+
+    componentDidMount() {
+        const searchedCity = "https://api.openweathermap.org/data/2.5/weather?q="+this.state.cityName+"&appid=4b0079617e152107616d2b410450dc2b"
+
+        fetch(searchedCity)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    weatherDetails: data
+                })
+            })
+    }
+
+    /**
+     * 
+     * @returns
+     * { 
+     * this.setState({
+     *    weatherDetails: data
+     * })
+     * }
+     */
+
+
     render() {
+        console.log(this.state.weatherDetails.weather)
+
         return (
             <div>
-                <CityName name={this.state.cityName} />
+                <CityName name={this.state.weatherDetails.name} />
                 <Pics />
-                <Temperature tempCelsius="25 Celsius"
+                <Temperature tempCelsius="22 Celsius"
                     tempFahrenheit="77 Fahrenheit" />
-                <Conditions conds="Rainy" />
+                <Conditions conds="blabla" />
                 <UpdateTime />
             </div>
         )
